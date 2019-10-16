696782ebcf35b9f4967c736de88dcebd6ea6ff2c
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-16 14:56:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,11 +9,14 @@ class School
   end
 
   def add_student(name, grade)
-    if @roster[grade] != nil
-      @roster[grade] << name
-    else
-      @roster[grade] = [name]
-    end
+    # if @roster[grade] != nil
+    #   @roster[grade] << name
+    # else
+    #   @roster[grade] = [name]
+    # end
+    # ||=“double-pipe equals” is an operator that assigns a value, much like = or our classic assignment operator, but will only complete the assignment if the left side of our operation returns false or nil.
+    roster[grade] ||= []
+    roster[grade] << name
   end
 
   def grade(grade)
